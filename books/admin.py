e19bac800241b74e941f6a6b05b9e26021139faf
SurajKarki <suraj.karki500@protonmail.com>
2022-12-13 18:47:56 +0545

    Implemented simple django admin inline

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book, Edition, Testimonial
+
+class EditionInline(admin.StackedInline):
+    model = Edition
+    extra = 0   # here extra: 0 means initially there will be no form for child model
+    fields = ['publisher', 'year', 'pages'] # specifying fields to show in the inline form
+
+class TestimonialInline(admin.StackedInline):
+    model = Testimonial
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [EditionInline, TestimonialInline] # list of inlines to show along with book form
+    list_display = ['title', 'author','user'] # list of columns to display in list of books page
+    readonly_fields = ['created_at', 'updated_at'] # these fields cannot be changed in the form
+    raw_id_fields = ['user'] # search a user from list of users
+
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class Book(models.Model):
     title = models.CharField(max_length=256)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     author = models.CharField(max_length=256)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Edition(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     publisher = models.CharField(max_length=256)
     year = models.IntegerField()
     pages = models.IntegerField()
-
 
 class Testimonial(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
